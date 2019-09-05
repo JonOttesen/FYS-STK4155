b0be7671d99f609133f7337beac761e328b9af05
Jon Andre Ottesen <joniandre1497@hotmail.com>
2019-09-05 15:11:29 +0200

    Changed from matrix inversion methode to SVD methode in OLS, and made a funtion for creating a plot equal to fig 2.11 as asked in c

--- Project1/regression.py
+++ Project1/regression.py
@@ -5,9 +5,10 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 from random import random, seed
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
-np.set_printoptions(precision = 3,suppress = True)
-m = 101
+#np.set_printoptions(precision = 3,suppress = True, threshold = np.inf)
+m = 31
 
 def FrankeFunction(x,y):
     a = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
@@ -17,16 +18,24 @@ def FrankeFunction(x,y):
 
     return a + b + c + d
 
-def plot3d(x, y, z):
+def plot3d(x, y, z, z2):
 
     fig = plt.figure()
-    ax = fig.gca(projection = '3d')
+    ax = fig.add_subplot(121, projection = '3d')
 
     surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
     linewidth=0, antialiased=False)
+    ax.set_zlim(-0.10, 1.40)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
-    ax.plot_surface(x, y, FrankeFunction(x, y),
+    ax = fig.add_subplot(122, projection = '3d')
+
+    ax.plot_surface(x, y, z2,
     linewidth=0, antialiased=False)
+    ax.set_zlim(-0.10, 1.40)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
     # Customize the z axis.
     ax.set_zlim(-0.10, 1.40)
@@ -53,22 +62,31 @@ class regression(object):
 
         x = np.ravel(self.x)
         y = np.ravel(self.y)
+        #design = ['1']
 
         m = len(x)
         X = np.ones((m, q))
 
         for i in range(1, p + 1):
             X[:, i] = x**i
-        index = i
+            #design.append('x^%.i' %(i))
+        if p == 0:
+            index = 0
+            i = 0
+        else:
+            index = i
 
         for i in range(1, l + 1):
             X[:, index + i] = y**i
+            #design.append('y^%.i' %(i))
         index += i
 
         for i in range(1, n + 1):
             for k in range(1, n + 1):
+                #design.append('x^%.i*y^%i' %(k, i))
                 X[:, index + k] = x**k*y**i
             index += k
+        #print(design)
 
         return X
 
@@ -92,17 +110,17 @@ class regression(object):
         R2D2 = 1 - np.sum((z - z_tilde)**2)/np.sum((z - np.mean(z))**2)
         return R2D2
 
-    def train_test(self, train = 0.75):
+    def train_test(self, train = 0.75, seed = 42):
         """
         Returns the test data and train data for the design matrix and for the z component
         X_train, X_test, z_train, z_test
         """
         z = np.ravel(np.copy(self.z))
-        X_train, X_test, z_train, z_test = train_test_split(self.X, z, train_size = train)
+        X_train, X_test, z_train, z_test = train_test_split(self.X, z, train_size = train, random_state = seed)
 
         return X_train, X_test, z_train, z_test
 
-    def OLS(self, z = 2, X = 'None'):
+    def OLS(self, z = 2, X = 'None', test = False):
         """
         Ordinary least squares up to order x^p, y^l and x^n*y^n.
         The general shape is [1, x^1, x^2 .., y^1, y^2 ...., x*y, x^(2)*y, x^n*y, x*y^2, x^2*y^2...]
@@ -111,11 +129,26 @@ class regression(object):
         n   -> Integers
         If X is given p, n and l won't matter, the polynomial degree used in X is used, X is typically used when using test data.
         """
+        if test:
+            reg = LinearRegression().fit(X, z)
+            return reg
         if type(X) == type('None'):
-            beta = np.linalg.inv(self.X.T.dot(self.X)).dot(self.X.T).dot(np.ravel(np.copy(self.z)))
+            U, s, V = np.linalg.svd(self.X)
+            sigma_inv = np.zeros(self.X.shape).T
+            sigma_inv[:len(s), :len(s)] = np.linalg.inv(np.diag(s))
+            z = np.ravel(self.z)
+
+            beta = V.T.dot(sigma_inv).dot(U.T).dot(z)
+            #beta = np.linalg.inv(self.X.T.dot(self.X)).dot(self.X.T).dot(np.ravel(np.copy(self.z)))
             return np.reshape(beta, (len(beta), 1))
         else:
-            beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(np.ravel(z))
+            U, s, V = np.linalg.svd(X)
+            sigma_inv = np.zeros(X.shape).T
+            sigma_inv[:len(s), :len(s)] = np.linalg.inv(np.diag(s))
+            z = np.ravel(z)
+
+            beta = V.T.dot(sigma_inv).dot(U.T).dot(z)
+            #beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(np.ravel(np.copy(z)))
             return np.reshape(beta, (len(beta), 1))
 
     def k_cross(self, X, z, folds, train = 0.8):
@@ -123,6 +156,9 @@ class regression(object):
             indexes = np.arange(len(X))
         except:
             X = np.copy(self.X)
+
+        if folds > len(X):
+            folds = len(X)
 
         length = len(X)
         pr_fold = int(length/folds)
@@ -145,7 +181,7 @@ class regression(object):
         k = 0
         train_indexes = []
         test_indexes = []
-        beta = np.zeros((len(X[0]), 1))
+        beta = []
         divider = 0
 
         for i in range(len(fold_indexes)):
@@ -156,7 +192,7 @@ class regression(object):
                     else:
                         train_indexes += fold[j]
 
-                beta += self.OLS(X = X[train_indexes], z = np.ravel(np.copy(z))[train_indexes])
+                beta.append(self.OLS(X = X[train_indexes], z = np.ravel(np.copy(z))[train_indexes]))
                 divider += 1
                 k = i
 
@@ -167,7 +203,8 @@ class regression(object):
             test_indexes = []
             train_indexes = []
 
-        beta /= divider
+        beta = np.array(beta)
+        beta = np.sum(beta, axis = 0)/len(beta)
         return beta
 
 
@@ -175,24 +212,79 @@ x = np.linspace(0, 1, m)
 y = np.linspace(0, 1, m)
 x, y = np.meshgrid(x, y)
 
-z = FrankeFunction(x, y)
-
-squares = regression(x, y, z, 5, 5, 5)
+z = FrankeFunction(x, y) #+ np.random.normal(0, 1, size = x.shape)
+"""
+squares = regression(x, y, z, 30, 30, 30)
 X_train, X_test, z_train, z_test = squares.train_test()
-beta = squares.OLS()
+reg = squares.OLS(X = X_train, z = z_train, test = True)
+z_tilde = reg.predict(X_train)
 beta_ols = squares.OLS(z = z_train, X = X_train)
-beta_k = squares.k_cross(X = X_train, z = z_train, folds = 15)
-z_tilde_ols = squares.z_tilde(beta_ols, X_test)
-z_tilde_k = squares.z_tilde(beta_k, X_test)
-z_tilde = squares.z_tilde(beta)
+z_tilde_ols = squares.z_tilde(beta_ols, X_train)
 
-print(squares.MSE(z_tilde, z), squares.R_squared(z_tilde, z))
-print(squares.MSE(z_tilde_ols, z_test), squares.R_squared(z_tilde_ols, z_test))
-print(squares.MSE(z_tilde_k, z_test), squares.R_squared(z_tilde_k, z_test))
+print(z_tilde)
+print(np.ravel(z_tilde_ols))
 
-#z_tilde = squares.z_tilde(beta = beta_k)
+"""
 
-plot3d(x, y, z_tilde)
+def fig_2_11(x, y, complexity = 10, N = 100):
+    errors_mse = np.zeros((2, complexity + 1))
+    errors_r = np.zeros((2, complexity + 1))
+
+    errors_mse_training = np.zeros((2, complexity + 1))
+    errors_r_training = np.zeros((2, complexity + 1))
+
+    complx = np.arange(0, complexity + 1, 1)
+
+    for k in range(N):
+        z = FrankeFunction(x, y) + np.random.normal(0, 1, size = x.shape)
+
+        for i in range(complexity + 1):
+            squares = regression(x, y, z, i, i, i)
+
+            X_train, X_test, z_train, z_test = squares.train_test(seed = 42)
+
+            beta_ols = squares.OLS(z = z_train, X = X_train)
+            beta_k = squares.k_cross(X = X_train, z = z_train, folds = 15)
+
+            z_tilde_ols = squares.z_tilde(beta_ols, X_test)
+            z_tilde_k = squares.z_tilde(beta_k, X_test)
+
+            errors_mse[0, i] += squares.MSE(z_tilde_ols, z_test)
+            errors_mse[1, i] += squares.MSE(z_tilde_k, z_test)
+            errors_r[0, i] += squares.R_squared(z_tilde_ols, z_test)
+            errors_r[1, i] += squares.R_squared(z_tilde_k, z_test)
+
+            z_tilde_ols = squares.z_tilde(beta_ols, X_train)
+            z_tilde_k = squares.z_tilde(beta_k, X_train)
+
+            errors_mse_training[0, i] += squares.MSE(z_tilde_ols, z_train)
+            errors_mse_training[1, i] += squares.MSE(z_tilde_k, z_train)
+            errors_r_training[0, i] += squares.R_squared(z_tilde_ols, z_train)
+            errors_r_training[1, i] += squares.R_squared(z_tilde_k, z_train)
+
+    #print(errors_mse)
+    #print(errors_mse_training)
+    errors_mse /= N
+    errors_r /= N
+    errors_mse_training /= N
+    errors_r_training /= N
+
+    plt.title('Regular OLS')
+    plt.plot(complx, errors_mse[0], label = 'Test')
+    plt.plot(complx, errors_mse_training[0], label = 'Training')
+    plt.ylim([0, np.max(errors_mse[0]*1.2)])
+    plt.legend()
+    plt.show()
+
+    plt.title('k-fold')
+    plt.plot(complx, errors_mse[1], label = 'Test')
+    plt.plot(complx, errors_mse_training[1], label = 'Training')
+    plt.ylim([0, np.max(errors_mse[1]*1.2)])
+    plt.legend()
+    plt.show()
+
+fig_2_11(x, y, complexity = 14)
+
 
 
 
